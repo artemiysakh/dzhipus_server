c94d86d45386457b00aba642ea7bd2cf98ffbed7
artemiysakh <artemiysakh0310@gmail.com>
2025-08-27 20:35:27 +1100

    MailService Commit

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -4,8 +4,8 @@ class MailService{
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT, //465
-            secure: false,
-            auth:{
+            secure: true, //было false
+             auth:{
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
             }
